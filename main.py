959ab6a8b108e02a5431318b0e1fcc5461eb2e3f
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-12-10 23:47:59 -0300

    feat: bot freelancer

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
-import discord
+import hikari
+from hikari import intents
 import asyncio
 import keyring
-from discord.ext import commands
 
 from engines.freelancer99 import get_freelancer99
 from engines.freelancer import get_freelancer
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = hikari.GatewayBot(keyring.get_password('bot_freelancer', 'token'), intents=intents.Intents.ALL)
 channel_id = 1144974807432183811
 
 sent_freelancer = []
 
-async def search_freelancer():
+@bot.listen()
+async def on_started(event: hikari.StartedEvent) -> None:
     '''
     Assynchronous function that searches for new jobs every 60 seconds and sends them to the Discord channel.
     The function searches for vacancies in the following websites:
-    * udemy
+    * 99freelas
+    * freelancer
+    * workana
     '''
-    await bot.wait_until_ready()
-    channel = bot.get_channel(channel_id)
-    while not bot.is_closed():
-        
-        # 99freelas
-        results = await get_freelancer99()
-        for result in results:
-            if result[0] not in sent_freelancer:
-                sent_freelancer.append(result[0])
-                freelancer_info = f'{"-"*50}\n\nTRABALHO: {result[1]}\nLINK: {result[3]}'
-                await channel.send(freelancer_info)
-                await asyncio.sleep(15)
-        await asyncio.sleep(30)
+  
+    # 99freelas
+    results = await get_freelancer99()
+    for result in results:
+        if result[0] not in sent_freelancer:
+            sent_freelancer.append(result[0])
+            freelancer_info = f'{"-"*50}\n\nTRABALHO: {result[1]}\nLINK: {result[3]}'
+            await bot.rest.create_message(channel_id, freelancer_info)
+            await asyncio.sleep(60)
+    await asyncio.sleep(30)
 
-        #freelancer
-        results = await get_freelancer()
-        for result in results:
-            if result[0] not in sent_freelancer:
-                sent_freelancer.append(result[0])
-                freelancer_info = f'{"-"*50}\n\nTRABALHO: {result[1]}\nLINK: {result[3]}'
-                await channel.send(freelancer_info)
-                await asyncio.sleep(15)
-        await asyncio.sleep(30)
+    #freelancer
+    results = await get_freelancer()
+    for result in results:
+        if result[0] not in sent_freelancer:
+            sent_freelancer.append(result[0])
+            freelancer_info = f'{"-"*50}\n\nTRABALHO: {result[1]}\nLINK: {result[3]}'
+            await bot.rest.create_message(channel_id, freelancer_info)
+            await asyncio.sleep(60)
+    await asyncio.sleep(30)
 
-        
-@bot.event
-async def on_ready():
-    bot.loop.create_task(search_freelancer())
-
-bot.run(keyring.get_password('bot_freelancer', 'token'))
+bot.run()
