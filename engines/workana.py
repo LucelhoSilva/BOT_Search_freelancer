f07bb9664009566c68f8eef8a3136ecd2e8cb5d5
TMMichaelsen <herrtmm@proton.me>
2023-12-12 00:42:41 -0300

    Refactor workana.py and add Workana engine to main.py
    Co-authored-by: LucelhoSilva <lucelhocristiano28@gmail.com>

--- engines/workana.py
+++ engines/workana.py
@@ -1,14 +1,38 @@
-import requests
+import httpx
 from bs4 import BeautifulSoup
+import json
+import hashlib
 
-def get_workana():
-  
-  url= 'https://www.workana.com/jobs?category=it-programming'
-  page = requests.get(url)
-  soup = BeautifulSoup(page.content, 'html.parser')
-  print(soup)
+async def get_workana():
+    '''
+    DOCSTRING
+    '''
 
-  cells = soup.find_all("div", id_="projects")
-  print(cells)
+    jobs = []
 
-get_workana()
+    for page in range(1):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f'https://www.workana.com/jobs?category=it-programming&language=pt&page={page}')
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+
+                cells = soup.find_all('div', class_='project-item')
+                for cell in cells:
+                    title = cell.find('h2').get_text(strip=True)
+                    date = cell.find('h5')['title']
+                    code = f'{title} - {date}'
+                    code = hashlib.sha256(code.encode()).hexdigest()
+
+                    skills = cell.find('div', class_='skills')
+                    to_expand = skills.find('label-expander').get(':to-expand')
+                    skills = json.loads(to_expand)
+                    stacks = [skill['anchorText'] for skill in skills]
+
+                    link = cell.find('a')['href']
+                    link = f'https://www.workana.com{link}'
+
+                    job = [code, title, stacks, link]
+                    jobs.append(job)
+
+    return jobs
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import keyring
 
 from engines.freelancer99 import get_freelancer99
 from engines.freelancer import get_freelancer
+from engines.workana import get_workana
 
 bot = hikari.GatewayBot(keyring.get_password('bot_freelancer', 'token'), intents=intents.Intents.ALL)
 channel_id = 1144974807432183811
@@ -17,8 +18,8 @@ async def on_started(event: hikari.StartedEvent) -> None:
     Assynchronous function that searches for new jobs every 60 seconds and sends them to the Discord channel.
     The function searches for vacancies in the following websites:
     * 99freelas
-    * freelancer
-    * workana
+    * Freelancer
+    * Workana
     '''
   
     # 99freelas
@@ -31,12 +32,22 @@ async def on_started(event: hikari.StartedEvent) -> None:
             await asyncio.sleep(60)
     await asyncio.sleep(30)
 
-    #freelancer
+    # Freelancer
     results = await get_freelancer()
     for result in results:
         if result[0] not in sent_freelancer:
             sent_freelancer.append(result[0])
             freelancer_info = f'{"-"*50}\n\nTRABALHO: {result[1]}\nLINK: {result[3]}'
+            await bot.rest.create_message(channel_id, freelancer_info)
+            await asyncio.sleep(60)
+    await asyncio.sleep(30)
+
+    # Workana
+    results = await get_workana()
+    for result in results:
+        if result[0] not in sent_freelancer:
+            sent_freelancer.append(result[0])
+            freelancer_info = f'{"-"*50}\n\nTRABALHO: {result[1]}\nSTACKS: {result[2]}\nLINK: {result[3]}'
             await bot.rest.create_message(channel_id, freelancer_info)
             await asyncio.sleep(60)
     await asyncio.sleep(30)
